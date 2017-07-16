d9b0b4c0560c8a0c363768c3cbe102f44ec64d28
askn <tc535mr2@gmail.com>
2017-07-16 10:04:56 +0900

    統合テストを追加

---
+++ test/AllTests.java
@@ -0,0 +1,17 @@
+import net.katagaitai.phpscan.MainTest;
+import net.katagaitai.phpscan.ast.PhpAstParserTest;
+import net.katagaitai.phpscan.compiler.CompilerTest;
+import net.katagaitai.phpscan.compiler.ProjectCompilerTest;
+import net.katagaitai.phpscan.interceptor.InterceptorTest;
+import net.katagaitai.phpscan.interpreter.InterpreterTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PhpAstParserTest.class, CompilerTest.class, ProjectCompilerTest.class, InterpreterTest.class,
+		InterceptorTest.class, MainTest.class })
+public class AllTests {
+
+}
